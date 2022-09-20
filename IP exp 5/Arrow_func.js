d2c408fb6e0b33f24c7a9f6c1a9b0024ae507c91
Nilay Pophalkar <nilaypophalkar@gmail.com>
2022-09-20 17:42:28 +0530

    arithematic operations using arrow functions

---
+++ IP exp 5/Arrow_func.js
@@ -0,0 +1,11 @@
+const add = (x, y) => x + y;
+console.log(`Addition: ${add(5, 9)}`);
+
+const multiply = (x, y) => x * y;
+console.log(`Multiplication: ${multiply(3, 4)}`);
+
+const divide = (x, y) => x / y;
+console.log(`Division: ${divide(3, 5)}`);
+
+const exponent = (base, power) => base ** power;
+console.log(`Exponent: ${exponent(2, 5)}`);
